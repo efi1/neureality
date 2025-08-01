c204db7b4f0f9beb57d6edeb3ad533048b23b2b3
efi1 <efovadia@gmail.com>
2025-08-02 01:13:21 +0300

    use global_config.json instead of settings.py.

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,16 +3,22 @@ import time
 import logging
 import json
 import docker
+from typing import List
 from pathlib import Path
 from importlib.abc import Traversable
 from _pytest.fixtures import fixture
 from importlib.resources import files
-from tests.cfg.cfg_global import settings
+from jinja2 import FileSystemLoader, Environment
 from tests.utils.data_to_obj import ObjectLikeData, data_object
 from tests.utils.collect_container_logs import collect_container_logs
 
 
 logger = logging.getLogger(__name__)
+
+CFG_DIR = Path().absolute().joinpath('tests', 'cfg')
+GLOBAL_CFG_DIR: Path = CFG_DIR.joinpath('cfg_global')
+GLOBAL_CFG_FILE: Path = Path(GLOBAL_CFG_DIR).joinpath("global_config.json")
+CFG_TEST_DIR: List[Path] = [CFG_DIR.joinpath(f) for f in ['cfg_non_parameterized_tests', 'cfg_parameterized_tests']]
 
 
 def is_container_healthy(container: object, timeout_seconds: int, interval: int = 1, elapsed: int = 0) -> bool:
@@ -69,32 +75,66 @@ def read_json_file(path: Path) -> dict:
     return data
 
 
-def share_get_data_logic(cfg_dir: str, test_name: str) -> ObjectLikeData:
-    cfg_file: Path | Traversable = files(cfg_dir).joinpath(test_name)
-    if cfg_file.exists():
-        json_params = read_json_file(cfg_file)
-        return data_object(json_params) # create an object with the test data
-    raise ValueError(f'Test {test_name} has no data – please check the test input file')
+def share_get_data_logic(test_name: str, global_dict_data: dict) -> ObjectLikeData:
+    is_file_exist = is_cfg_file(test_name, CFG_TEST_DIR)
+    if not is_file_exist:
+        logger.info(f'Test {test_name} has no data, executing test with no data file')
+        return None
+    test_template = get_cfg_template(test_name, CFG_TEST_DIR)
+    cfg_data = test_template.render(global_dict_data)
+    dict_data = json.loads(cfg_data)
+    return data_object(dict_data)
 
 
-@fixture(scope="function")
-def load_test_data(request) -> ObjectLikeData:
-    """
-    Rendering config data out of a template cfg file - for non parameterized test
-    :return: tests data as a class object
-    """
-    test_name = request.node.name
-    logging.info(F"load cfg_data for {test_name}")
-    cfg_dir = settings.non_parameterized_tests_dir
-    return share_get_data_logic(cfg_dir, f'{test_name}.json')
-
-
-def get_param_data(test_name: str) -> ObjectLikeData:
+def get_param_data(test_name: str, global_dict_data: dict) -> ObjectLikeData:
     """
     Rendering config data out of a template cfg file - for parameterized test
+    :param global_dict_data: global data
     :param test_name: name as given by test when it is being executed
     :return: tests data as a class object
     """
-    logging.info(F"load cfg_data for {test_name.split('.')[0]}")
-    cfg_dir = settings.parameterized_tests_dir
-    return share_get_data_logic(cfg_dir, test_name)
+    logging.info(F"load cfg_data for {test_name}")
+    return share_get_data_logic(test_name, global_dict_data)
+
+
+def get_cfg_template(test_name, cfg_test_dir: List[Path | Traversable]):
+    template_loader = FileSystemLoader(searchpath=cfg_test_dir)
+    template_env = Environment(loader=template_loader)
+    template = template_env.get_template(f'{test_name}.json')
+    return template
+
+
+def is_cfg_file(test_name, cfg_test_dir) -> bool:
+    cfg_test_name = F'{test_name}.json'
+    for path in cfg_test_dir:
+        cfg_test_path = path.joinpath(cfg_test_name)
+        if cfg_test_path.exists():
+            return True
+    return False
+
+
+@fixture(scope="session")
+def global_dict_data(file=GLOBAL_CFG_FILE):
+    return read_json_file(file)
+
+
+@fixture(scope="function")
+def load_test_data(request, global_dict_data: dict) -> ObjectLikeData:
+    """
+    Rendering config data out of a template cfg file - for unparameterized test
+    :param request: fixture utility to get the test name
+    :param global_dict_data: project's global_cfg data
+    :return: tests data as a class object
+    """
+    test_name = request.node.name
+    return share_get_data_logic(test_name, global_dict_data)
+
+
+def pytest_generate_tests(metafunc):
+    if 'test_name' in metafunc.fixturenames:
+        file_path = GLOBAL_CFG_FILE
+        config = read_json_file(file_path)
+        param_dir = files(config['parameterized_tests_dir'])
+        test_names = [p.name.split('.')[0] for p in param_dir.iterdir() if p.is_file()]
+        metafunc.parametrize('test_name', test_names)
+
--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -2,8 +2,6 @@ from typing import Tuple
 import pytest
 import requests
 from requests import Response
-from tests.cfg.cfg_global import settings
-from importlib.resources import files
 from conftest import get_param_data, logger
 from tests.utils.data_to_obj import data_object, ObjectLikeData, object_dump
 
@@ -13,24 +11,21 @@ def shared_test_logic(cfg_data: ObjectLikeData) -> Tuple[Response, ObjectLikeDat
     req_type = task_data.request_type # GET or POST request
     expected_data = cfg_data.return_data # expected response of the request
     api_path = cfg_data.api_path
-    uri = f'{settings.base_url}{api_path}'
-    log_msg = f'calling get {uri}' if req_type == 'get' else f'calling post {uri}, data: {object_dump(task_data)}'
+    log_msg = f'calling get {api_path}' if req_type == 'get' else f'calling post {api_path}, data: {object_dump(task_data)}'
     logger.info(F"api-client request: {log_msg}")
     # execute the api call
-    response = requests.get(uri) if req_type == 'get' else requests.post(uri, json=object_dump(task_data))
+    response = requests.get(api_path) if req_type == 'get' else requests.post(api_path, json=object_dump(task_data))
     return response, expected_data
 
 
-@pytest.mark.parametrize('test_name', [resource.name
-                                        for resource in files(settings.parameterized_tests_dir).iterdir()])
-def test_perform_tasks(app_container: object, test_name: str) -> None:
+def test_perform_tasks(app_container: object, test_name: str, global_dict_data: dict) -> None:
     """
     Testing various task using the files in app.cfg.cfg_parameterized_tests as an input (parameterized test)
     :param app_container: the docker container in which the app resides in
     :param test_name: test name as taken from the file names resides in tests.cfg.cfg_parameterized_tests folder
     :return: no return - assertion is made on the api response
     """
-    cfg_data: data_object = get_param_data(test_name=test_name) # get the test data
+    cfg_data: data_object = get_param_data(test_name, global_dict_data) # get the test data
     resp: Tuple[Response, ObjectLikeData] = shared_test_logic(cfg_data)
     response, expected_data = resp
     res_json = response.json()
@@ -94,3 +89,4 @@ def test_negative_405_wrong_request_method(app_container: object, load_test_data
     if expected_data.validate_resp_val:
         assert res_json.get('result') == expected_data.return_value, (F"\nwrong response val: {res_json.get('result')}\n"
                                                                  F"expected: {expected_data.return_value}\n")
+
