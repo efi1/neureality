152e6ea7dbfb73600db58e6354f83bc826a71cb3
efi1 <efovadia@gmail.com>
2025-08-21 11:30:58 +0300

    MANIFEST.in required when building the repo and install the requirements.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-def parse_requirements(filename)-> list:
+def parse_requirements(filename) -> list:
     with open(filename, 'r') as f:
         return [line.strip() for line in f if line and not line.startswith("#")]
 
@@ -8,17 +8,20 @@ setup(
     name='NeuRealityProject',
     version='0.1.0',
     description='NeuReality task assignment',
-    packages=find_packages(where="."),  # searches from current directory
-    package_dir={"": "."},              # maps root to actual package structure
+    packages=find_packages(where="."),
+    package_dir={"": "."},
+    include_package_data=True,
     package_data={
         "tests.cfg.cfg_global": ["*.json"],
         "tests.cfg.cfg_parameterized_tests": ["*.json"],
         "tests.cfg.cfg_non_parameterized_tests": ["*.json"]
     },
-
-    # metadata
     author='',
     author_email='',
     license='proprietary',
     install_requires=parse_requirements('requirements.txt'),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
 )
