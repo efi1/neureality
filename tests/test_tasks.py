123902932481d991f80171b095e676d909998143
efi1 <efovadia@gmail.com>
2026-09-18 14:37:46 +0300

    add failure test

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -91,3 +91,9 @@ def test_negative_405_wrong_request_method(app_container: object, load_test_data
         assert expected_data.return_value in res_json, (F"\nwrong response val: {res_json.get('result')}\n"
                                                                  F"expected: {expected_data.return_value}\n")
 
+def test_should_fail():
+    assert 1 == 2, "Expected 1 to be 2, but it was not"
+
+
+# def test_should_fail2():
+#     assert False, "Expected to False"
